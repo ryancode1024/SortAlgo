c9c131201348d119e9d6758e47f447235053e950
ryancode1024 <ryancode1024@gmail.com>
2022-10-06 18:45:50 +0800

    feat: update

--- test/sort_algo_test.cpp
+++ test/sort_algo_test.cpp
@@ -5,8 +5,15 @@
 
 using namespace std;
 
-int main(int arvc, char *argv[]) {
-  vector<int> nums = {1, 4, 3, 93, 2};
+int main(int args, const char *argv[]) {
+  // vector<int> nums = {1, 4, 3, 93, 2};
+  vector<int> nums;
+  nums.push_back(1);
+  nums.push_back(4);
+  nums.push_back(3);
+  nums.push_back(93);
+  nums.push_back(2);
+
   sort(nums.begin(), nums.end());
   for (int i = 0; i < nums.size(); i ++ ) {
     std::cout << nums[i] << std::endl;
